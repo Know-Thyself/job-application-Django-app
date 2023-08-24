dfa89c8bba65d13ae001ead2dfa0463c0356713a
Biruk Kebede <biruk.key@gmail.com>
2023-08-25 00:14:10 +0100

    Admin Panel customisation

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import ApplicationFormModel
 
-admin.site.register(ApplicationFormModel)
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "occupation")
+    list_filter = ("date", "occupation")
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(ApplicationFormModel, ApplicationAdmin)
