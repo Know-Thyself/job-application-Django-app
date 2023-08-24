1a5bc8eaf73e3dd157c6ea71f66fbfda113ec9e4
Biruk Kebede <biruk.key@gmail.com>
2023-08-24 02:55:18 +0100

    Database model and migration

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ApplicationForm(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    email = models.EmailField()
+    date = models.DateTimeField("start date")
+    occupation = models.CharField(max_length=80)
+
+    def __str__(self) -> dict:
+        applicant = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "date": self.date,
+            "occupation": self.occupation,
+        }
+        return applicant
